068f472b85c6e897265457e590b6d2c4743cb513
Oliver Gorwits <oliver@cpan.org>
2025-04-16 22:22:09 +0100

    release 1.001000

--- lib/Alien/ultraviolet.pm
+++ lib/Alien/ultraviolet.pm
@@ -7,7 +7,7 @@ use parent qw( Alien::Base );
 use Path::Tiny;
 use File::ShareDir::Dist qw( dist_share );
 
-our $VERSION = '1.000000';
+our $VERSION = '1.001000';
 
 sub bin_dir { path( dist_share __PACKAGE__ )->child('bin') }
 
